f16b13a31ff950b81f28bb7f3d95d43d1a456a29
Luan Rohwedder <luan-rohwedder@hotmai.com>
2024-06-26 02:56:46 -0400

    Inserindo CORS para requisições frontend

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,15 @@ import router from "./routes/router";
 import * as dotenv from 'dotenv';
 import {PrismaClient} from '@prisma/client'
 import prisma from "./db";
+import cors from 'cors';
 
 dotenv.config();
 const PORT = process.env.API_PORT
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json())
 
 app.use(router)
